2fc92359a9380b0c8ec21578258632bed135270a
ki_mimang <ki_mimang@ki-mimangui-MacBookAir.local>
2025-05-17 22:07:08 +0900

    feat: 커스텀 예외 추가 및 운동 영상 구현 중

---
+++ src/main/java/com/kimnjin/gwanyeon/commons/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kimnjin.gwanyeon.commons.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+
+}
--- src/main/java/com/kimnjin/gwanyeon/exercisevideo/entity/ExerciseVideo.java
+++ src/main/java/com/kimnjin/gwanyeon/exercisevideo/entity/ExerciseVideo.java
@@ -1,5 +1,31 @@
 package com.kimnjin.gwanyeon.exercisevideo.entity;
 
-public class ExerciseVideo {
+
+import com.kimnjin.gwanyeon.commons.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Setter
+public class ExerciseVideo extends BaseEntity {
+
+  Long exerciseVideoId;
+
+  Long userId;
+
+  String title;
+
+  String url;
+
+  String youtubeId;
+
+  String description;
 
 }
