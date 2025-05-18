1a523454f461ad176f14d3ae5f817f918ae1af71
ki_mimang <ki_mimang@ki-mimangui-MacBookAir.local>
2025-05-18 22:26:20 +0900

    fix: 의미없는 콜론 삭제

--- src/main/java/com/kimnjin/gwanyeon/exercisevideo/service/impl/ExerciseVideoServiceImpl.java
+++ src/main/java/com/kimnjin/gwanyeon/exercisevideo/service/impl/ExerciseVideoServiceImpl.java
@@ -54,7 +54,6 @@ public class ExerciseVideoServiceImpl implements ExerciseVideoService {
       throw new BadRequestException(BAD_REQUEST);
 
     }
-    ;
 
     return ExerciseVideoResponseDto.from(exerciseVideo);
 
