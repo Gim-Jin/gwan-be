25748492848827c0ae6afcded16b6f161523473a
ki_mimang <ki_mimang@ki-mimangui-MacBookAir.local>
2025-05-25 19:48:48 +0900

    feat: gpt 답변 일정하게 들어오는거 수정

--- src/main/java/com/kimnjin/gwanyeon/gpt/service/GptService.java
+++
@@ -1,9 +0,0 @@
-package com.kimnjin.gwanyeon.gpt.service;
-
-import com.kimnjin.gwanyeon.rehabprogram.dto.UserSurveyRequestDto;
-
-public interface GptService {
-
-  String getGptResponse(UserSurveyRequestDto userSurveyRequestDto);
-  
-}
