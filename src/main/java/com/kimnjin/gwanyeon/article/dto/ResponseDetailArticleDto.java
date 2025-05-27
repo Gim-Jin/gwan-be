b4e3345875e1333efd19adacc7675f5d398e46fa
ki_mimang <ki_mimang@ki-mimangui-MacBookAir.local>
2025-05-28 00:41:28 +0900

    feat: 커뮤니티 관련 기능 수정

--- src/main/java/com/kimnjin/gwanyeon/article/dto/ResponseDetailArticleDto.java
+++ src/main/java/com/kimnjin/gwanyeon/article/dto/ResponseDetailArticleDto.java
@@ -2,9 +2,7 @@ package com.kimnjin.gwanyeon.article.dto;
 
 import com.kimnjin.gwanyeon.article.entity.Article;
 import com.kimnjin.gwanyeon.review.dto.ResponseReviewDto;
-import com.kimnjin.gwanyeon.review.entity.Review;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,17 +12,20 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ResponseDetailArticleDto {
+
   private Long articleId;
   private Long userId;
   private String userName;
   private String title;
   private String content;
   private List<ResponseReviewDto> reviews;
+
   private LocalDateTime createTime;
   private LocalDateTime updateTime;
 
 
-  public static ResponseDetailArticleDto from(Article article, Long userId, String userName, List<ResponseReviewDto> reviews) {
+  public static ResponseDetailArticleDto from(Article article, Long userId, String userName,
+      List<ResponseReviewDto> reviews) {
     ResponseDetailArticleDto responseDetailArticleDto = new ResponseDetailArticleDto();
     responseDetailArticleDto.articleId = article.getArticleId();
     responseDetailArticleDto.userId = userId;
